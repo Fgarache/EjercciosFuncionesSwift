10d531ab2b0f31f3248a9cfce06b257c773a6700
Francisco Garache <franciscogarach3@gmail.com>
2022-05-27 18:38:18 +0000

    hyt

--- main.swift
+++ main.swift
@@ -1,3 +1,14 @@
 /*Ejercicio 1
 Escribir una función que aplique un descuento a un precio y otra que aplique el IVA a un precio. Escribir una tercera función que reciba un diccionario con los precios y porcentajes de una cesta de la compra, y una de las funciones anteriores, y utilice la función pasada para aplicar los descuentos o el IVA a los productos de la cesta y devolver el precio final de la cesta.*/
 
+class producto {
+var producto = "Audifonos"
+var descuento = 10
+var precio = 60
+let IVA = 15    
+}
+
+
+func descuento (cantidad: Int, descuento: Int, iva: Int, precio: Int) -> Int{
+    let total = precio * cantidad
+}
